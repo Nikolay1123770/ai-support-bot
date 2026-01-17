ab9d681fec58b0a4ff6a9f0f5958832b282a2a7f
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2026-01-18 00:32:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
 
-# --- НАСТРОЙКИ БОТА ---
+# Твой токен бота
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7869311061:AAGPstYpuGk7CZTHBQ-_1IL7FCXDyUfIXPY") 
 
-# Твой токен от @BotFather
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7869311061:AAGPstYpuGk7CZTHBQ-_1IL7FCXDyUfIXPY") # <--- ЗАМЕНИ НА СВОЙ ТОКЕН ЕСЛИ ЭТОТ ПРИМЕР
-
-# Твой ключ Groq (Вставлен твой рабочий ключ)
+# Твой ключ Groq
 GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
 
-# ID Админа (чтобы работали команды /stats и /send)
-# Замени на свой цифровой ID (можно узнать у @userinfobot)
-ADMIN_ID = int(os.getenv("ADMIN_ID", "8473513085")) 
+# ID Админа
+ADMIN_ID = int(os.getenv("ADMIN_ID", "8473513085"))
